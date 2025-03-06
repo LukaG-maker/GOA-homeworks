b4a736903a018e0dc4657dfc044a26ef57856f1d
LukaG-maker <levangvelukashvili@gmail.com>
2025-03-06 05:36:18 -0800

    commit

---
+++ day 08/classwork/classwork.py
@@ -0,0 +1,20 @@
+#Name = "nika"
+# naMe= "luka"
+# NAME= "data"
+# naME= "vano"
+# name= "demetre"
+# naME= "jan"
+# name="giorgi"
+# 
+# 6 იმიტომ რომ ყველანი ცვლადები არიან
+# 
+# 
+# 
+name_name="luka"
+name_1="demetre"
+name_2="giorgi"
+name_3="vano"
+my_favorite_car="lamborghini"
+my_favorite="luka"
+
+
---
+++ day 08/homework/lesson7.py
@@ -0,0 +1,6 @@
+num1=8
+num2=16
+num3=24
+num4=32
+num5=40
+print((num1+num2+num3+num4+num5) /5)
