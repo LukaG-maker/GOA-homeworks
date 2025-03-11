30677fb62eff3abf1accd5e3ff21127a7d9e45cb
LukaG-maker <levangvelukashvili@gmail.com>
2025-03-11 09:41:57 -0700

    commit

--- day 01/classwork/classwork.py
+++ day 01/classwork/classwork.py
@@ -1,1 +1,37 @@
 #არაფერი გვქონდა
+name= "Luka"
+
+
+surname = "gvelukashvili"
+
+
+
+name = "Luka" #ეს არის str (string) ტიპის ცვლადი
+age = 12 # ეს არის int (integer) მთელი რიცხვი
+height = 171.5 # ეს არის Float ტიპის ცვლადი ( ათწილადი)
+#boolean (bool) ტიპის ცვლადი
+
+knows_programming = True   #True ან False
+is_ugly = False #snakecase( უბრალოდ წერის სტილი სტანდარტულად)
+
+isugly = False # ჯავასკრიპტული camelcase
+
+
+print(name + " " + surname)
+
+
+
+print(type(age))
+print(type(name))
+print(type(surname))
+print(type(height))
+print(type(knows_programming))
+
+print(name + " " + str(age))
+
+print(  name + " " + surname )
+
+
+
+
+print(  name + " " + surname + " " + str(age) + " " +  str(height)  ) 
--- day 01/homework/lesson1.py
+++ day 01/homework/lesson1.py
@@ -1,43 +1,9 @@
 name= "Luka"
+surname="gvelukashvili"
+country="georgia"
+height=1.73
+weight=50
 
+heyitsme=( name + " " + surname + " " + country + " " + str(height) +" "+ str(weight))
+print(heyitsme)
 
-
-#name არის ცვლადი
-# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
-# "Luka" არის ცვლადისთვის მნიშვნელობა
-
-surname = "gvelukashvili"
-
-# print(name)
-#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
-
-name = "Luka" #ეს არის str (string) ტიპის ცვლადი
-age = 12 # ეს არის int (integer) მთელი რიცხვი
-height = 171.5 # ეს არის Float ტიპის ცვლადი ( ათწილადი)
-#boolean (bool) ტიპის ცვლადი
-
-knows_programming = True   #True ან False
-is_ugly = False #snakecase( უბრალოდ წერის სტილი სტანდარტულად)
-
-isugly = False # ჯავასკრიპტული camelcase
-
-
-print(name + " " + surname)
-
-#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
-# print(name + age)
-
-# print(type(age))
-# print(type(name))
-# print(type(surname))
-# print(type(height))
-# print(type(knows_programming))
-
-print(name + " " + str(age))
-
-print(  name + " " + surname )
-
-
-
-
-print(  name + " " + surname + " " + str(age) + " " +  str(height)  )  
