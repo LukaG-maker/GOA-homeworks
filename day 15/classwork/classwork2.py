87930d6be7825f0df034153b7501be652568e80b
LukaG-maker <levangvelukashvili@gmail.com>
2025-03-27 09:04:25 -0700

    commit

---
+++ day 15/classwork/classwork2.py
@@ -0,0 +1,5 @@
+my_username= "luka"
+
+user_nameis = input("enter your name : ")
+
+print(my_username == user_nameis)
---
+++ day 15/homework/homework.py
@@ -0,0 +1,8 @@
+registration = input("Enter your email and password : ") 
+registration = False
+
+registration = input("Email and password invalid try againm : ") 
+registration = True
+
+
+
