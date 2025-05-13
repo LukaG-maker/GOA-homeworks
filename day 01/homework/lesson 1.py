6f9744cf445583464981d8989f43c6713c124b2a
LukaG-maker <levangvelukashvili@gmail.com>
2025-05-13 15:04:10 +0400

    commit

---
+++ day 01/homework/lesson 1.py
@@ -0,0 +1,36 @@
+name  =  "iva"
+
+#name არის ცვლადი
+#= არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+#ივა არის ცვლადისთვის მნიშვნელობა
+
+surname  =  "lazarishvili"
+
+print(name)
+print(surname)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+
+name = "iva"  #ეს არის str-strins ტიპის ცვლადი
+age = 37 #ეს არის int (integer) მთელი რიცხვი 
+heigh = 180  #ეს არის float ტიპის ცვლადი(ანუ ათწილადი)
+#boolean  (bool)  ტიპის ცვლადი
+
+knows_programming = True  #true ან false
+is_ugly = False
+
+#print(name + " " + surname)
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(heigh))
+#print(type(knows_programming))
+
+print(name + " " + surname + " "  + str(heigh) + " " + str(knows_programming) + " " + str(age) + " " + str(is_ugly))
+
+
+
